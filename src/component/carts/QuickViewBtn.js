46d2b7130fca3f486bd8f8e95f4f34e6f1519078
amir <amirrezazadeh.job@gmail.com>
2025-12-05 13:29:01 +0330

    finish quick view modal

--- src/component/carts/QuickViewBtn.js
+++ src/component/carts/QuickViewBtn.js
@@ -3,11 +3,11 @@ export default function QuickViewBtn({ id, onQuickview }) {
     <button onClick={() => onQuickview(id)} className="p-1.5 cursor-pointer relative group/item">
       <span className="stroke-gray hover:stroke-org fill-white">
         <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-          <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+          <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+          <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
           <g id="SVGRepo_iconCarrier">
             <g id="Interface / Search_Magnifying_Glass">
-              <path id="Vector" d="M15 15L21 21M10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10C17 13.866 13.866 17 10 17Z" stroke-width="1.08" stroke-linecap="round" stroke-linejoin="round"></path>{" "}
+              <path id="Vector" d="M15 15L21 21M10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10C17 13.866 13.866 17 10 17Z" strokeWidth="1.08" strokeLinecap="round" strokeLinejoin="round"></path>
             </g>
           </g>
         </svg>
