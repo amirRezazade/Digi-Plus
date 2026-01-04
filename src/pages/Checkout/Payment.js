88019efb4c51760784a74260e2a5c38a9d35c0bf
amir <amirrezazadeh.job@gmail.com>
2026-01-04 20:25:15 +0330

    start payment success page

--- src/pages/Checkout/Payment.js
+++ src/pages/Checkout/Payment.js
@@ -1,50 +1,108 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 export default function Payment({ price }) {
   let expirationYears = new Date().getFullYear();
-  console.log(expirationYears);
+
+  let [cardNumber, setCardNumber] = useState([]);
+  let [cardHolder, setCardHolder] = useState("");
+  let [cardYear, setCardYear] = useState("");
+  let [cardMonth, setCardMonth] = useState("");
+  let [cardCvv, setCardCvv] = useState("");
+  let [cardPassword, setCarPassword] = useState("");
+  let [loading, setLoading] = useState(false);
+  const formatCardNumber = (value) => {
+    return value
+      .replace(/\D/g, "") // حذف غیر عدد
+      .slice(0, 16) // حداکثر 16 رقم
+      .replace(/(.{4})/g, "$1 ") // فاصله بعد هر 4 رقم
+      .trim();
+  };
+  function sumbitForm(e) {
+    e.preventDefault();
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      window.location.href = "/";
+    }, 2000);
+  }
 
   return (
-    <div className="w-screen min-h-screen bg-[#ddeefc] flex justify-center items-center">
-      <div className="mt-20 bg-white rounded-md w-140 min-h-100 relative overflow-visible select-none">
-        <div className="absolute w-8/10 h-70 rounded-2xl overflow-hidden bg-[url(https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/8.jpeg)] bg-cover top-0 left-1/2 -translate-1/2 shadow-[3px_13px_30px_0px_#0b132980]">
+    <div className="w-screen min-h-screen  flex justify-center items-center py-20">
+      <div className="mt-30 bg-white rounded-lg border border-light-gray/70 gray-shaddow w-140 min-h-100 relative  overflow-visible select-none">
+        <div className="absolute w-8/10 h-65 rounded-2xl overflow-hidden bg-[url(https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/8.jpeg)] bg-cover top-0 left-1/2 -translate-1/2 shadow-[3px_13px_30px_0px_#0b132980]">
           <div className="absolute w-full h-full top-0 left-0 text-white bg-black/40 p-6 flex flex-col justify-between ">
             <div className="flex justify-between items-center">
               <img className="max-w-18" src="https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/visa.png" alt="" />
+              <div className="flex items-center gap-1 ms-auto me-4">
+                <span className="tracking-widest">{cardCvv || ""}</span>
+                <span className="text-white/80">: CVV</span>
+              </div>
               <img className="max-w-15" src="https://raw.githubusercontent.com/muhammederdem/credit-card-form/master/src/assets/images/chip.png" alt="" />
             </div>
-            <div className="w-full text-2xl font-bold flex justify-around items-center  gap-2 tracking-widest">
-              <span>####</span>
-              <span>####</span>
-              <span>####</span>
-              <span>####</span>
+            <div dir="ltr" className="w-full text-2xl font-bold flex justify-center items-center  gap-5 tracking-widest">
+              <div>
+                <span className="w-4.5 inline-block">{cardNumber[0] || "#"}</span>
+                <span className="w-4.5 inline-block">{cardNumber[1] || "#"}</span>
+                <span className="w-4.5 inline-block">{cardNumber[2] || "#"}</span>
+                <span className="w-4.5 inline-block">{cardNumber[3] || "#"}</span>
+              </div>
+              <div>
+                <span className="w-4.5 inline-block">{cardNumber[5] ? "*" : "#"}</span>
+                <span className="w-4.5 inline-block">{cardNumber[6] ? "*" : "#"}</span>
+                <span className="w-4.5 inline-block">{cardNumber[7] ? "*" : "#"}</span>
+                <span className="w-4.5 inline-block">{cardNumber[8] ? "*" : "#"}</span>
+              </div>
+              <div>
+                <span className="w-4.5 inline-block">{cardNumber[10] ? "*" : "#"}</span>
+                <span className="w-4.5 inline-block">{cardNumber[11] ? "*" : "#"}</span>
+                <span className="w-4.5 inline-block">{cardNumber[12] ? "*" : "#"}</span>
+                <span className="w-4.5 inline-block">{cardNumber[13] ? "*" : "#"}</span>
+              </div>
+              <div>
+                <span className="w-4.5 inline-block">{cardNumber[15] || "#"}</span>
+                <span className="w-4.5 inline-block">{cardNumber[16] || "#"}</span>
+                <span className="w-4.5 inline-block">{cardNumber[17] || "#"}</span>
+                <span className="w-4.5 inline-block">{cardNumber[18] || "#"}</span>
+              </div>
             </div>
             <div className="w-full flex justify-between items-center text-base">
               <div className="">
                 <span className="text-white/60">Expires</span>
                 <div>
-                  <span>MM</span>/<span>YY</span>
+                  <span>{cardMonth || "MM"}</span>/<span>{cardYear ? cardYear[2] + cardYear[3] : "YY"}</span>
                 </div>
               </div>
-              <div className="grow text-end">
+
+              <div className="grow text-end max-w-80">
                 <span className="text-white/60">Card Holder</span>
-                <div>
-                  <span className="text-lg">Full Name</span>
+                <div className="truncate">
+                  <span className="text-lg truncate">{cardHolder || "Full Name"}</span>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div dir="ltr" className="mt-40 p-5 w-full text-gray text-sm">
-          <h2 className="text-center text-black text-2xl font-black ">243,000 $</h2>
+        <form dir="ltr" className="mt-35 p-5 w-full text-gray text-sm" onSubmit={sumbitForm}>
+          <h2 className="text-center text-dark text-2xl sm:text-3xl lg:text-4xl font-black ">243,000 $</h2>
           <div>
-            <label htmlFor="cart-number">cart Number</label>
-            <input className="w-full text-lg mt-2 border border-light-gray rounded-lg focus:border-blue-400 outline-0 p-3 " type="number" id="cart-number" maxLength={16} minLength={16} autocomplete="off" />
+            <label htmlFor="cart-number">Card Number</label>
+            <input className="w-full text-lg mt-2 border border-light-gray rounded-lg focus:border-blue-400 outline-0 p-3 " type="text" id="cart-number" maxLength={19} minLength={19} required autoComplete="off" inputMode="numeric" value={cardNumber} onInput={(e) => setCardNumber(formatCardNumber(e.target.value.trim()))} />
+          </div>
+          <div className="mt-3">
+            <label htmlFor="cart-holder">Card Holder</label>
+            <input className="w-full text-lg mt-2 border border-light-gray rounded-lg focus:border-blue-400 outline-0 p-3 " type="text" id="cart-holder" minLength={3} required autoComplete="off" value={cardHolder} onInput={(e) => setCardHolder(e.target.value)} />
+          </div>
+          <div className="mt-3">
+            <label htmlFor="password">Password</label>
+            <input className="w-full text-lg mt-2 border border-light-gray rounded-lg focus:border-blue-400 outline-0 p-3 " type="password" id="password" minLength={4} maxLength={4} required inputMode="numeric" pattern="[0-9]*" autoComplete="off" value={cardPassword} onInput={(e) => setCarPassword(e.target.value.trim().replace(/\D/g, ""))} />
           </div>
           <div className="flex flex-col xs:flex-row gap-5 justify-between items-center ">
             <div className="grow my-5">
               <label htmlFor="expiration-mount">Expiration Date</label>
               <div className="flex items-center gap-3 mt-1 text-base">
-                <select className="grow outline-0 border border-light-gray focus:border-blue-400 rounded-lg p-3" id="expiration-mount" value={null}>
-                  <option value="null" disabled selected>
+                <select required value={cardMonth} className="grow outline-0 border border-light-gray focus:border-blue-400 rounded-lg p-3" id="expiration-mount" onChange={(e) => setCardMonth(e.target.value)}>
+                  <option value="" disabled>
                     Mount
                   </option>
                   <option value="01">01</option>
@@ -60,8 +118,8 @@ export default function Payment({ price }) {
                   <option value="11">11</option>
                   <option value="12">12</option>
                 </select>
-                <select className="grow outline-0 border border-light-gray focus:border-blue-400 rounded-lg p-3" id="expiration-year" value={null}>
-                  <option value="null" disabled selected>
+                <select required value={cardYear} className="grow outline-0 border border-light-gray focus:border-blue-400 rounded-lg p-3" id="expiration-year" onChange={(e) => setCardYear(e.target.value)}>
+                  <option value="" disabled>
                     Year
                   </option>
                   <option value={expirationYears}>{expirationYears}</option>
@@ -80,14 +138,21 @@ export default function Payment({ price }) {
             </div>
             <div className="xs:max-w-1/3">
               <label htmlFor="cart-cvv">CVV</label>
-              <input className="w-full  text-lg mt-2 border border-light-gray rounded-xl focus:border-blue-400 outline-0 p-3 " type="number" id="cart-cvv" maxLength={4} minLength={3} autocomplete="off" />
+              <input className="w-full  text-lg mt-2 border border-light-gray rounded-xl focus:border-blue-400 outline-0 p-3 " type="number" id="cart-cvv" maxLength={4} minLength={3} required inputMode="numeric" pattern="[0-9]*" autoComplete="off" value={cardCvv} onInput={(e) => e.target.value.trim().length < 5 && setCardCvv(e.target.value.trim())} />
             </div>
           </div>
           <div className="flex items-center justify-between gap-4 text-white">
-            <button className="py-3 grow bg-red-700 hover:bg-red-600 rounded-2xl cursor-pointer">انصراف</button>
-            <button className="py-3 grow-2 bg-green-700 hover:bg-green-600 rounded-2xl cursor-pointer">پرداخت</button>
+            <Link to={"/"} className="py-3 w-3/10 text-center bg-red-700 hover:bg-red-600 rounded-2xl cursor-pointer">
+              انصراف
+            </Link>
+            <button type="submit" disabled={loading} className="py-3 w-7/10 bg-green-700 hover:bg-green-600 rounded-2xl cursor-pointer disabled:cursor-wait disabled:opacity-50">
+              {loading ? <span className="border-4 border-transparent border-t-white rounded-full block mx-auto size-5 animate-spin"></span> : "پرداخت"}
+            </button>
           </div>
-        </div>
+          <span dir="rtl" className="text-xs pt-3 text-center block ">
+            این درگاه صرفاً جهت تست و شبیه‌سازی پرداخت است.
+          </span>
+        </form>
       </div>
     </div>
   );
